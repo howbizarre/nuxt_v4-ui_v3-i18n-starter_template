a47ce13b23891b2f1e1cd0b5d795087ce84c7cbf
howbizarre <howbizarre@gmail.com>
2025-03-25 09:28:06 +0000

    Add indent to Setings

---
+++ app/layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+  <UApp class="relative">
+    <NuxtRouteAnnouncer />
+    <main class="sm:pl-[50px] sm:pb-0 max-sm:pl-0 max-sm:pb-[50px]">
+      <ColorModeButton />
+      <LangSwitcher />
+
+      <UContainer class="pt-3 pb-3 sm:pb-6 lg:pb-8 mx-0">
+        <slot />
+      </UContainer>
+    </main>
+  </UApp>
+</template>
+
+<style>
+body,
+html {
+  @apply flex flex-col;
+}
+
+html {
+  @apply h-full w-full;
+}
+
+body {
+  @apply min-h-full min-w-full bg-gray-200 dark:bg-gray-800 relative;
+}
+</style>
