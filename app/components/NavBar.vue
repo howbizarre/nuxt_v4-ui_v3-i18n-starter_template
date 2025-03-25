d9989f7b968a1abaa184bdbafdf3a1f1e94a9ef1
howbizarre <howbizarre@gmail.com>
2025-03-25 13:03:04 +0000

    UPD

--- app/components/NavBar.vue
+++ app/components/NavBar.vue
@@ -6,12 +6,12 @@ const localePath = useLocalePath();
 <template>
   <nav class="flex justify-between items-center px-4 py-2 rounded-xl bg-gray-200 dark:bg-gray-800">
     <div class="flex items-center gap-6 sm:gap-4"">
-      <NuxtLinkLocale :to="localePath('/')" class="flex items-center gap-1">
+      <NuxtLinkLocale :to="localePath('/')" class="flex items-center gap-1 py-0.5">
         <UIcon name="i-heroicons-home" class="size-4.5" />
         <span class="hidden sm:block">{{ t('LBL_HOME') }}</span>
       </NuxtLinkLocale>
 
-      <NuxtLinkLocale :to="localePath('/info')" class="flex items-center gap-1">
+      <NuxtLinkLocale :to="localePath('/info')" class="flex items-center gap-1 px-2 py-0.5">
         <UIcon name="i-heroicons-information-circle" class="size-5" />
         <span class="hidden sm:block">{{ t('LBL_INFO') }}</span>
       </NuxtLinkLocale>
