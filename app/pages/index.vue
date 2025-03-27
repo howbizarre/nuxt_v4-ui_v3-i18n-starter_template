0829ff54720ca64acb0c3f3dac6a55f56c007b19
howbizarre <howbizarre@gmail.com>
2025-03-27 12:12:18 +0000

    ADD: Nuxt Content

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,9 +1,14 @@
 <script lang="ts" setup>
 const { t } = useI18n();
+const localePath = useLocalePath();
+const { data: home } = await useAsyncData(() => queryCollection("content").path(localePath("/")).first());
 </script>
 
 <template>
   <UContainer class="my-3 border-t p-4 border-gray-200 dark:border-gray-800 rounded-xl">
     {{ t('LBL_HELLO') }}
+
+    <ContentRenderer v-if="home" :value="home" />
+    <div v-else>Home not found</div>
   </UContainer>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 import { fileURLToPath } from 'url';
 
 export default defineNuxtConfig({
-  modules: ["@nuxt/ui", "@nuxtjs/i18n"],
+  modules: ["@nuxt/ui", "@nuxtjs/i18n", "@nuxt/content"],
 
   i18n: {
     vueI18n: "./i18n.config.ts",
