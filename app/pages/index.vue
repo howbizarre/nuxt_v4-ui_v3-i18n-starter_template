998b5115001a1baeda039df8add564c3710adbc5
howbizarre <howbizarre@gmail.com>
2025-03-27 14:46:49 +0000

    Generate

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,14 +3,27 @@ const { t } = useI18n();
 const localePath = useLocalePath();
 const thisPath = computed(() => localePath("/"));
 
-const { data: home } = await useAsyncData(() => queryCollection("content").path(thisPath.value).first());
+const { data: page } = await useAsyncData(() => queryCollection("content").path(thisPath.value).first());
+
+watch(thisPath, async () => {
+  page.value = await queryCollection("content").path(thisPath.value).first();
+  await nextTick();
+});
 </script>
 
 <template>
   <UContainer class="my-3 border-t p-4 border-gray-200 dark:border-gray-800 rounded-xl">
     {{ t('LBL_HELLO') }} {{ thisPath }}
 
-    <ContentRenderer v-if="home" :value="home" />
-    <div v-else>Home not found</div>
+    <template v-if="page">
+      <ContentRenderer :value="page" />
+    </template>
+    <template v-else>
+      <div class="empty-page">
+        <h1>Page Not Found</h1>
+        <p>Oops! The content you're looking for doesn't exist.</p>
+        <NuxtLink to="/">Go back home</NuxtLink>
+      </div>
+    </template>
   </UContainer>
 </template>
--- nuxt4bp/_nuxt/CVEtaWwE.js
+++
@@ -1,1 +0,0 @@
-import{f as o,g as a,h as n,o as r,w as s,d as _,t as c,i as d,j as p}from"./B1jiL6e4.js";const l=o({__name:"info",setup(i){const{t:e}=a();return(u,f)=>{const t=p;return r(),n(t,{class:"my-3 border-t p-4 border-gray-200 dark:border-gray-800 rounded-xl"},{default:s(()=>[_(c(d(e)("LBL_INFO")),1)]),_:1})}}});export{l as default};
