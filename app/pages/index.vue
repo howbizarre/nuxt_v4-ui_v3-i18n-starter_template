c40d4e1934dc25d38e89acc2da21177199b45ac3
howbizarre <howbizarre@gmail.com>
2025-03-27 15:21:13 +0000

    регенерате

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,13 +2,9 @@
 const { t } = useI18n();
 const localePath = useLocalePath();
 const thisPath = computed(() => localePath("/"));
-
 const { data: page } = await useAsyncData(() => queryCollection("content").path(thisPath.value).first());
 
-watch(thisPath, async () => {
-  page.value = await queryCollection("content").path(thisPath.value).first();
-  await nextTick();
-});
+watch(thisPath, async () => { await nextTick(); });
 </script>
 
 <template>
